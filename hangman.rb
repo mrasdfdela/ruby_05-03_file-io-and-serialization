5f5ffe2b1aab86da9fbbfa28e71c383c0bb5310e
mrasdfdela <jerryhsu830@gmail.com>
2020-12-03 00:09:07 -0500

    Implemented save and load features w/ yaml serialization

--- hangman.rb
+++ hangman.rb
@@ -1,37 +1,65 @@
 require 'byebug'
+require 'yaml'
 
 class Hangman
-  attr_reader :player, :board, :secret_word
+  attr_reader :player, :board, :guessed_letters, :saved_directory
   def initialize
-    @player = Player.new
     @board = Board.new
+    @guessed_letters = []
+    @saved_directory = 'saved_directory.txt'
   end
 
   def play_game
-    puts 'Let\'s play a game'
+    start_game
     until @board.incorrect_count == 6 || @board.word_guessed
       board.print_board
-      guess = player.get_guess
-      board.eval_guess(guess)
+      guess = get_guess
+
+      if guess == 'save'
+        save_game
+        break
+      else
+        @guessed_letters << guess
+        board.eval_guess(guess)
+      end
     end
-    end_of_game
-    byebug
+    end_game
   end
 
-  def end_of_game
+  def start_game
+    puts "Let\'s play hangman!"
+    puts "Enter 'n' to start a new game or 'l' to load a game:"
+    entry = ''
+    until entry == 'l' || entry == 'n'
+      entry = gets.downcase.chomp
+    end
+    entry == 'l' ? load_game : @player = get_name
+  end
+
+  def load_game
+    puts "Select from the list of saved games:"
+    File.open(saved_directory, 'r').readlines.each { |line| puts line }
+
+    file = ''
+    until File.exists?("#{file}.yml")
+      puts "Enter the game name:"
+      file = gets.chomp
+    end
+    s = YAML.load(File.read("#{file}.yml"))
+
+    @player = s.player
+    @guessed_letters = s.guessed_letters
+    @board = s.board
+  end
+
+  def end_game
     if @board.word_guessed
-      puts "Congratulation! You have won!"
-    else
-      puts "Sorry, you have run out of guesses. =("
+      puts "Congratulation, #{@player}! You have won!"
+    elsif @board.incorrect_count == 6
+      puts "Sorry, #{@player}, you have run out of guesses. =("
+    else 
+      puts 'Game saved!'
     end
-  end
-end
-
-class Player
-  attr_reader :name, :guessed_letters
-  def initialize
-    @name = get_name
-    @guessed_letters = []
   end
 
   def get_name
@@ -40,23 +68,48 @@ class Player
   end
 
   def get_guess
-    msg = 'Please guess a letter:'
+    msg = "Please guess a letter, or enter 'save':"
     g = ''
-    until g.length == 1 && g.between?('a', 'z') && !guessed_letters.include?(g)
+
+    until g.length == 1 && g.between?('a', 'z') && !@guessed_letters.include?(g) || g == 'save'
       p msg
       g = gets.chomp.downcase
       msg = "\nInvalid selection. Please guess again:"
     end
-    @guessed_letters << g
     g
+  end
+
+  def save_game
+    # p = @player
+    # t = Time.now.strftime("%Y%m%d_%H%M%S")
+    # save_file = File.open("#{p}_#{t}.yml", 'w') {|file|
+    #   file.write(self.to_yaml)
+    # }
+
+    # file = 'saved_games.yaml'
+    # Dir.mkdir file unless File.exists?('saved_games.yml') { |file|
+    #   file.(self.to_yaml)
+    # }
+
+    save_file = ''
+    save_file = gets.chomp
+    File.open("#{save_file}.yml", 'w') { |file|
+      file.write(self.to_yaml)
+    }
+
+    d = @saved_directory
+    File.open(d,'w') unless File.exists?(d)
+    File.open(d, 'a') {|file|
+      file.puts save_file
+    }
   end
 end
 
 class Board
   attr_reader :word_bank, :secret_word, :board_guesses, :incorrect_count, :guesses
   def initialize
-    @word_bank = parse_word_bank
-    @secret_word = select_secret_word(@word_bank, 5,12)
+    # @word_bank = parse_word_bank
+    @secret_word = select_secret_word(parse_word_bank, 5,12)
     @board_guesses = populate_board(@secret_word)
     @incorrect_count = 0
     @guesses = []
